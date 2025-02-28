8c10f0b860c4c06e3b85507c9c45f55e302fd5aa
amitruls1 <amitchauh4n@gmail.com>
2025-02-28 15:29:00 +0530

    bad code

---
+++ utils/utils.ts
@@ -0,0 +1,26 @@
+// Example of bad JavaScript code
+
+function veryBadFunctionName(x, y) {
+  if (x > 10) {
+    if (y < 5) {
+      if (x + y > 15) {
+        return "bad";
+      } else {
+        return "worse";
+      }
+    }
+  }
+  return "terrible";
+}
+
+var abc =
+  "this is a very long string that should probably be a template literal or split up in some way because it is too long for a single line of code but whatever lets keep going";
+
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+const unusedVar = 42;
+
+console.log("This code is pretty bad");
